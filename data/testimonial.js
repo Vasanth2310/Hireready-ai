5a265a57e4c91f040df8c9f8a67089fbe94fad24
Vasanth Kumar C <vasanth49468@gmail.com>
2025-05-30 12:24:00 +0530

    One Last Time

--- data/testimonial.js
+++ data/testimonial.js
@@ -3,7 +3,7 @@ export const testimonial = [
     quote:
       "HireReady AI's smart job matching helped me discover roles I would’ve never found on my own. It felt like having a personal career coach!",
     author: "Alicia Gomez",
-    image: "https://randomuser.me/api/portraits/women/75.jpg",
+    image: "https://randomuser.me/api/portraits/women/70.jpg",
     role: "UX Designer",
     company: "Creative Minds Studio",
   },
@@ -11,7 +11,7 @@ export const testimonial = [
     quote:
       "The mock interviews felt incredibly real. I walked into my actual interview more confident and prepared than ever before.",
     author: "Jason Lee",
-    image: "https://randomuser.me/api/portraits/men/75.jpg",
+    image: "https://randomuser.me/api/portraits/men/70.jpg",
     role: "Data Analyst",
     company: "InsightWorks",
   },
@@ -19,7 +19,7 @@ export const testimonial = [
     quote:
       "HireReady AI revamped my resume in minutes and helped me stand out to recruiters. I landed three interviews in the first week!",
     author: "Neha Sharma",
-    image: "https://randomuser.me/api/portraits/women/74.jpg",
+    image: "https://randomuser.me/api/portraits/women/64.jpg",
     role: "Operations Manager",
     company: "LogiChain Solutions",
   },
